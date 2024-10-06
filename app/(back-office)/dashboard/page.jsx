b8b12e4b0baa6fae675e51bccf175086b0dbfe9f
AdriALV2005 <adrimon7221@gmail.com>
2024-10-06 16:21:33 -0500

    Small Cards

--- app/(back-office)/dashboard/page.jsx
+++ app/(back-office)/dashboard/page.jsx
@@ -1,11 +1,13 @@
 import Heading from "@/components/backoffice/Heading";
 import LargeCards from "@/components/backoffice/LargeCards";
+import SmallCards from "@/components/backoffice/SmallCards";
 import React from "react";
 
 function page() {
   return <div>
     <Heading  title="Dashboard Overwiev"/>
     <LargeCards />
+    <SmallCards />
   </div>;
 }
 
